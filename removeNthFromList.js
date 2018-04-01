2fa7191c4c0d2ce7d6a4768599d603f01e834a8b
julweng <julweng@gmail.com>
2018-03-31 22:31:21 -0700

    remove nth from last of list

---
+++ removeNthFromList.js
@@ -0,0 +1,73 @@
+// Write an algorithm to find the nth element from the end of a 
+// linked list without using the .length property and remove it
+// Write an algorithm to find the third element from the end of a 
+// linked list without using the .length property
+
+class Node {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  
+  addToHead(val) {
+    const newNode = new Node(val);
+    if(this.head) {
+      newNode.next = this.head;
+    }
+    this.head = newNode;
+  }
+
+  remove(val) {
+    let node = this.head;
+    while (node.next) {
+      if (node.next.val === val) {
+        node.next = node.next.next;
+      }
+      node = node.next;
+    }
+  }
+}
+
+function getNthFromLast(n, list) {
+  let fast = list.head;
+  let slow = list.head;
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow.val;
+}
+
+function removeNthFromLast(n, list) {
+  let nth = getNthFromLast(n, list);
+  list.remove(nth);
+  return list;
+}
+
+const list = new LinkedList();
+list.addToHead(1);
+list.addToHead(5);
+list.addToHead(4);
+list.addToHead(7);
+list.addToHead(10);
+list.addToHead(3);
+list.addToHead(6);
+
+console.log(list)
+console.log(removeNthFromLast(3, list))
+
+let node = list.head;
+while (node) {
+  node = node.next;
+  console.log(node)
+}
